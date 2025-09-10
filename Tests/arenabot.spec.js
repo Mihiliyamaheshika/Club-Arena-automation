410fadaca045158d617db128c5fbe681085deaa7
Mihiliya Maheshika <110465573+Mihiliyamaheshika@users.noreply.github.com>
2025-09-10 10:48:47 +0530

    Create arenabot.spec.js
    Arena bot execution checking

---
+++ Tests/arenabot.spec.js
@@ -0,0 +1,37 @@
+const { test, expect } = require('@playwright/test');
+
+
+test('Arena bot testing', async ({ page }) => {
+    await page.goto('https://club-arena-test2-bhancvd6ghf9dwd7.westeurope-01.azurewebsites.net/about');
+    await page.locator("//a[normalize-space()='Log in']").click();
+
+    const email = 'mihiliyamaheshika@gmail.com';
+    const password = 'Mm1234';
+
+    // Check if email format is valid using regex
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        throw new Error(`Invalid email format: ${email}`);
+    }
+
+    await page.getByRole('textbox', { name: 'Email' }).fill(email);
+    await page.getByLabel('Password', { exact: true }).fill(password);
+    await page.waitForSelector('button', { timeout: 5000 });
+    await page.getByRole('button', { name: 'Log in' }).click();
+
+    await expect(page).toHaveURL('https://club-arena-test2-bhancvd6ghf9dwd7.westeurope-01.azurewebsites.net/clubs');
+
+    await page.locator("//span[normalize-space()='Help center']").click();
+    await expect(page).toHaveURL('https://club-arena-test2-bhancvd6ghf9dwd7.westeurope-01.azurewebsites.net/help-center');
+
+
+    await page.getByText('FAQ').click();
+    await page.locator('button.mdc-fab mat-icon').click();
+    await page.locator('.mat-mdc-dialog-actions > .mat-mdc-form-field > .mat-mdc-text-field-wrapper > .mat-mdc-form-field-flex > .mat-mdc-form-field-infix').click();
+
+
+    await page.getByRole('textbox', { name: 'What\'s on your mind?' }).fill('how I use this application ');
+    await page.getByRole('textbox', { name: 'What\'s on your mind?' }).press('Enter');
+  
+
+});
